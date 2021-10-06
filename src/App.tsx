a3d5a29e3654b7c318b6d54f4b5b07c068b3119a
Asher <greatasher7@gmail.com>
2021-10-06 15:29:08 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App(): JSX.Element {
+  const good = 1;
+
+  return (
+    <>
+      <h1>App Conponent</h1>
+      <h2>dsafad</h2>
+      <p>sdaf</p>
+    </>
+  );
+}
+
+export default App;
