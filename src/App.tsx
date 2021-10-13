55459a6d6887932c0d2ec34fb768c5011e9b83b4
Asher <greatasher7@gmail.com>
2021-10-13 11:33:03 +0900

    add file-loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
 import React from 'react';
 
 function App(): JSX.Element {
-  const good = 1;
-
   return (
     <>
       <h1>App Conponent</h1>
-      <h2>dsafad</h2>
-      <p>sdaf</p>
     </>
   );
 }
